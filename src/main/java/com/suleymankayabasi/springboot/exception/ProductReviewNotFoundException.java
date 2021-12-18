e282d3acc5e7ba381a9040aca2211ebb38e8fbc1
Süleyman Kayabaşı <82765835+suleymankayabasi@users.noreply.github.com>
2021-12-19 01:37:03 +0300

    Add Exception Handling

---
+++ src/main/java/com/suleymankayabasi/springboot/exception/ProductReviewNotFoundException.java
@@ -0,0 +1,12 @@
+package com.suleymankayabasi.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductReviewNotFoundException extends RuntimeException {
+
+    public ProductReviewNotFoundException(String message){
+        super(message);
+    }
+}
